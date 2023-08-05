3930922ea30593abefbdd1a6e85ade6e02332242
Chris Woodby <chrisdwoodby@gmail.com>
2023-08-04 23:17:25 -0500

    environment set up

---
+++ src/Main.jsx
@@ -0,0 +1,15 @@
+import Navigation from './Navigation.jsx';
+import Home from './Home.jsx';
+import Footer from './Footer.jsx';
+
+export default function Main(props) {
+
+    return (
+        <div>
+            <h1>hello world</h1>
+            {/* <Navigation/>
+            <Home/>
+            <Footer/> */}
+        </div>
+    )
+}
