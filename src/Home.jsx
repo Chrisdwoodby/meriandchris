c88a6327fa99d8c763be9fa0e564697b652c73d9
Chris Woodby <chrisdwoodby@gmail.com>
2023-11-07 18:39:27 -0600

    site developement complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,18 +3,84 @@ import React, {useState} from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import map from '../media/map.png';
-import colorPallet from '../media/color-pallet.jpg';
-import walking from '../media/walking.png';
+import photoshoot from '../media/photoshoot.png';
 import { ElfsightWidget } from 'react-elfsight-widget';
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselItem from 'react-bootstrap/CarouselItem';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import CarouselCaption from 'react-bootstrap/CarouselCaption';
 
 
 export default function Home(props) {
+
+  window.addEventListener('scroll', function() {
+    var element = document.querySelector('#date-outer');
+    var position = element.getBoundingClientRect();
+
+    // checking whether fully visible
+    if(position.top >= 0 && position.bottom <= window.innerHeight) {
+        element.classList.add("countdown-in-view");
+    }
+
+    // checking for partial visibility
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+    }
+});
+
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('.home-paragraph-2');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if(position.top >= 0 && position.bottom <= window.innerHeight) {
+
+  }
+
+  // checking for partial visibility
+  if(position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add("boxes-loaded");
+  }
+});
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('.home-paragraph-2');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if(position.top >= 0 && position.bottom <= window.innerHeight) {
+      
+  }
+
+  // checking for partial visibility
+  if(position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add("boxes-loaded");
+  }
+});
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('.schedule');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if(position.top >= 0 && position.bottom <= window.innerHeight) {
+      element.classList.add("schedule-loaded");
+  }
+
+  // checking for partial visibility
+  if(position.top < window.innerHeight && position.bottom >= 0) {
+  }
+});
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('.home-paragraph-3');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if(position.top >= 0 && position.bottom <= window.innerHeight) {
+      
+  }
+
+  // checking for partial visibility
+  if(position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add("where-to-stay-loaded");
+  }
+});
+
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -57,45 +123,51 @@ export default function Home(props) {
         <div className="home-banner">
           <Col className="bannerLeft" md={6}>
 
-          <h2 className="form-title"><b>Please confirm your email address</b> so that we can let you know when it is time to RSVP.</h2>
+          <h2 className="form-title">We are excited to have you be<br></br> a part of our special day!</h2>
             <ElfsightWidget className="form" widgetID="7c116791-a047-4a26-8dd8-2cb68dece206" />
+          <h2 className="form-subtitle"><b>Please confirm your email address</b> so that we can let you know when it is time to RSVP.</h2>
           </Col>
           <Col className="bannerRight" md={6}>
               <div className="above-knot">
-                 <span>Saturday, April 6, 2024</span>
+                 <span >Saturday, April 6, 2024</span>
               </div>
               <div className="below-knot">
-                  <span>1000 Southcreek Dr <br></br>Round Rock, TX 78664</span>
+                  <span >1000 Southcreek Dr. <br></br>Round Rock, TX 78664</span>
               </div>
           </Col>
         </div>
         <div id="date-outer">
-                <h2 className="headings">Countdown to Forever</h2>
+                <h2 className="headings countdown-title">Countdown to Forever</h2>
                 <span id="countdown"></span>
         </div>
-        {/* <div className="home-paragraph ">
+        <div className="schedule">
           <Container>
-            <Row className="rowreverse">
-              <Col>
-                <h3 className="headings">The Venue | Kinningham House</h3>
+            <Row>
+              <Col id="schedule" md={5} className="schedule-col">
+                <div className="schedule-item-list fade-in-top">
+                  <h2>The Schedule</h2>
+                  <br></br>
+                  <span className="schedule-item">Guest Arrival - <b>3:30 PM</b></span>
+                  <span className="schedule-item">Ceremony - <b>4:00 PM</b></span>
+                  <span className="schedule-item">Cocktail Hour - <b>Late Afternoon</b> </span>
+                  <span className="schedule-item">Dinner - <b>Evening</b></span>
+                  <span className="schedule-item">Dancing - <b>Evening</b></span>
+                  <span className="schedule-item">After-Party at The Domain - <b>Late Evening</b></span>
+                </div>
               </Col>
-              <Col md={6}>
+              <Col md={7} className="schedule-col-right">
+                <img src={photoshoot}/>
               </Col>
             </Row>
           </Container>
-        </div> */}
+        </div>
         <div className="home-paragraph-2">
           <Container>
             <Row className="message-cols-home">
-            <Col className="message-cols box shadow" md={5}>
+            <Col className="message-cols box shadow fade-in-left" md={5}>
                   <h3 className="headings">The Venue | Kinningham House</h3>
                   <article>
-                  The NEW development features 6,700 square feet of indoor
-                  and outdoor rental event space available for use by the
-                  public this fall. The facility will also host numerous
-                  Parks and Rec youth programs throughout the year. 
-                  This $2.3 million rebuild of the facility is in 
-                  Kinningham Park.
+                  The Kinningham House is an event space situated within Kinningham Park and managed by the Round Rock Parks and Recreation Department. It includes both indoor space and covered outdoor space. The bay doors that run along two sides of the building can be opened to connect with the outdoors when the weather is pleasant. The space is used for youth programs throughout the year.
                   </article>
                   <button className="cta" onClick={handleShow}>View on Map</button>
                   <Modal show={show} onHide={handleClose}>
@@ -111,12 +183,10 @@ export default function Home(props) {
                     </Modal.Footer>
                   </Modal>
               </Col>
-              <Col className="message-cols box shadow" md={5}>
+              <Col className="message-cols box shadow fade-in-right" md={5}>
                   <h3 className="headings" id="what-to-wear">What to Wear</h3>
                   <article>
-                    While it is not a requirement do do so, we are asking our guests 
-                    to bring our color pallet alive by choosing to wear colors like
-                    what you see here on our website! 
+                  We invite you to help us bring our wedding colors to life by wearing one of the pastel colors or deeper colors in the circles below. The dress code is dressy, encompassing dresses of any type, suits of any type, and button-down shirts or blouses with slacks or skirts.
                   </article>
                   
                   <div className="color-pallet-container">
@@ -125,7 +195,28 @@ export default function Home(props) {
                     <div className="color-pallet bright-pink"></div>
                     <div className="color-pallet blue"></div>
                     <div className="color-pallet green"></div>
+                    <div className="color-pallet dark-green"></div>
                   </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="home-paragraph-3">
+          <Container>
+            <Row className="message-cols-home">
+              <Col md={12} className="message-cols box shadow fade-in-bottom">
+                  <h3 className="headings" id="accommodations">Tips for Accommodations</h3>
+                  <article>
+                  Round Rock is a city just north of Austin, so if you would prefer to 
+                  stay near our venue, you can look at accommodations in either Round
+                   Rock or north Austin, both of which are great areas. Three hotels 
+                   near the venue with higher ratings on Hotels.com are
+                   <a className="hotel-link" href="https://www.marriott.com/en-us/hotels/austn-towneplace-suites-austin-round-rock/overview/" target="_blank"> TownePlace Suites 
+                   Austin Round Rock</a>, <a className="hotel-link" href="https://www.ihg.com/avidhotels/hotels/us/en/round-rock/ausav/hoteldetail" target="_blank">avid hotel Austin – Round Rock South</a>, and
+                   <a className="hotel-link" href="https://www.ihg.com/holidayinnexpress/hotels/us/en/round-rock/rndes/hoteldetail" target="_blank"> Holiday Inn Express and Suites Round Rock - Austin N</a>. Plus, you can find even more options on other sites.
+                  </article> 
+                  <br></br>
+                  <article><b className="headings">After-Party:</b> We will be having an informal after-party at The Domain in north Austin (Rock Rose Avenue, Austin, TX 78758). Consider how this fits in with your plans to use a rideshare like Uber or Lyft or stay in a specific location. It may be more economical to take a late-night rideshare than to stay near The Domain.</article>   
               </Col>
             </Row>
           </Container>
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -19,6 +19,20 @@ export default function Navigation(props) {
       elmntToView.scrollIntoView();  
     }, 500) 
   }
+  const scrollToSchedule = () => {
+    handleClose()
+    var elmntToView = document.getElementById("schedule");
+    setTimeout(() => {
+      elmntToView.scrollIntoView();  
+    }, 500) 
+  }
+  const scrolltoAccommodations = () => {
+    handleClose()
+    var elmntToView = document.getElementById("accommodations");
+    setTimeout(() => {
+      elmntToView.scrollIntoView();  
+    }, 500) 
+  }
 
   return (
     <div className="header-bar">
@@ -36,20 +50,21 @@ export default function Navigation(props) {
     </Container> */}
     <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Menu</Offcanvas.Title>
+          <Offcanvas.Title id="menu-title" >Have a Look Around</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body className="menu-body">
           <div className="modal-body">
-            <a class="main-menu-item" onClick={scrollToWhatToWear}><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
-</svg>When and Where</a>
+            
+<a class="main-menu-item" onClick={scrollToSchedule}><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 7V12L9.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>Schedule</a>
             <a class="main-menu-item" onClick={scrollToWhatToWear}><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3 7L6 4H9C9 4.39397 9.0776 4.78407 9.22836 5.14805C9.37913 5.51203 9.6001 5.84274 9.87868 6.12132C10.1573 6.3999 10.488 6.62087 10.8519 6.77164C11.2159 6.9224 11.606 7 12 7C12.394 7 12.7841 6.9224 13.1481 6.77164C13.512 6.62087 13.8427 6.3999 14.1213 6.12132C14.3999 5.84274 14.6209 5.51203 14.7716 5.14805C14.9224 4.78407 15 4.39397 15 4H18L21 7L20.5 12L18 10.5V20H6V10.5L3.5 12L3 7Z" stroke="#000000" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
 </svg>What to Wear</a>
-            <a class="main-menu-item"><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12 7V12L9.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>Schedule</a>
-            <a class="main-menu-item" href="https://www.zola.com/registry/christopherandmeri" target="_blank"><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <a class="main-menu-item" onClick={scrolltoAccommodations}><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3 21H5M5 21H10M5 21V3M10 21H14M10 21V16L8 16C10 13.3333 14 13.3333 16 16L14 16V21M14 21H19M19 21H21M19 21V3M3 3H5M5 3H19M19 3H21M9 6.5H10M14 6.5H15M9 10.5H10M14 10.5H15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>Tips for Accommodations</a>
+<a class="main-menu-item" href="https://www.zola.com/registry/christopherandmeri" target="_blank"><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M12 7V20M12 7H8.46429C7.94332 7 7.4437 6.78929 7.07533 6.41421C6.70695 6.03914 6.5 5.53043 6.5 5C6.5 4.46957 6.70695 3.96086 7.07533 3.58579C7.4437 3.21071 7.94332 3 8.46429 3C11.2143 3 12 7 12 7ZM12 7H15.5357C16.0567 7 16.5563 6.78929 16.9247 6.41421C17.293 6.03914 17.5 5.53043 17.5 5C17.5 4.46957 17.293 3.96086 16.9247 3.58579C16.5563 3.21071 16.0567 3 15.5357 3C12.7857 3 12 7 12 7ZM5 12H19V17.8C19 18.9201 19 19.4802 18.782 19.908C18.5903 20.2843 18.2843 20.5903 17.908 20.782C17.4802 21 16.9201 21 15.8 21H8.2C7.07989 21 6.51984 21 6.09202 20.782C5.71569 20.5903 5.40973 20.2843 5.21799 19.908C5 19.4802 5 18.9201 5 17.8V12ZM4.6 12H19.4C19.9601 12 20.2401 12 20.454 11.891C20.6422 11.7951 20.7951 11.6422 20.891 11.454C21 11.2401 21 10.9601 21 10.4V8.6C21 8.03995 21 7.75992 20.891 7.54601C20.7951 7.35785 20.6422 7.20487 20.454 7.10899C20.2401 7 19.9601 7 19.4 7H4.6C4.03995 7 3.75992 7 3.54601 7.10899C3.35785 7.20487 3.20487 7.35785 3.10899 7.54601C3 7.75992 3 8.03995 3 8.6V10.4C3 10.9601 3 11.2401 3.10899 11.454C3.20487 11.6422 3.35785 11.7951 3.54601 11.891C3.75992 12 4.03995 12 4.6 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>Registry</a>
           </div>
