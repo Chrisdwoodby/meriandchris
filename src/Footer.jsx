acdc5429b125b9704f49fc7af30128271d7bfaa1
Chris Woodby <chrisdwoodby@gmail.com>
2023-11-16 22:34:29 -0600

    registry text updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -14,7 +14,8 @@ export default function Footer () {
                  </Col>
                  <Col className="registry" md={6}>
                    <h3>Our Registry</h3>
-                   <article>As our wedding date gets closer, there will be more items to choose between on our registry. Thank you for thinking about ways to commemorate our milestone!</article>
+                   <article>Having you be a part of our wedding is already a gift, but if you would like to commemorate our milestone in an additional way,
+                      you can refer to our registry. As our wedding date gets closer, there will be more items to choose between. Thank you for celebrating with us!</article>
                    <a id="registry-btn" className="cta" href="https://www.zola.com/registry/christopherandmeri" target="_blank">View Registry</a>
                  </Col>
              </Row>
